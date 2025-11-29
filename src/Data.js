89dc12eb485e23ea8b436be420bfdd3a6213c38d
yang2706 <yangob27@gmail.com>
2025-11-29 15:45:31 +0700

    hi

--- src/Data.js
+++ src/Data.js
@@ -1,156 +1,179 @@
- export const item = [
-   {
-      id: 1,
-      title: 'Use Force',
-      img: 'https://m.media-amazon.com/images/M/MV5BNTgzMjg0NGYtZTI0OC00OWViLWJjOWYtNDVhMjUxMGZhNTgzXkEyXkFqcGc@._V1_.jpg',
-      rating: 8.2,
-      desc: 'Two African American teens become a target of racial profiling by local Law Enforcement.'
-    },
+ export const Mydata = [
     {
-      id: 2,
-      title: 'Tom Hardy',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/04/Havoc.jpg',
-      rating: 9.8,
-      desc: 'With his breakthrough performance as Eames in Christopher Nolan s sci-fi thriller Inception (2010)'
-    },
-    {
-      id: 3,
-      title: 'In Your Dreams',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/In-Your-Dreams-610x903.jpg',
-      rating: 7.6,
-      desc: 'Stevie and her little brother Elliot journey into the wildly absurd landscape of their own dreams to ask the Sandman to grant them the perfect family.'
-    },
-    {
-      id: 4,
-      title: 'Monster Island Movie',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/07/Monster-Island-Movie-610x893.jpg',
-      rating: 9.1,
-      desc: 'Set in the Pacific, 1942. A Japanese soldier and a British prisoner of war are stranded on a deserted island, hunted by a deadly creature. Two mortal enemies must come together to survive the unknown.'
-    },
-    {
-      id: 5,
-      title: 'Frankenstein',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/Frankenstein.jpg',
-      rating: 6.9,
-      desc: 'Dr. Victor Frankenstein, a brilliant but egotistical scientist, brings a creature to life in a monstrous experiment that ultimately leads to the undoing of both the creator and his tragic creation.'
-    },
-    {
-      id: 6,
-      title: 'Osiris',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/Osiris-610x813.jpg',
-      rating: 7.9,
-      desc: 'Special Forces commandos are abducted mid-operation by a mysterious spacecraft and, upon awakening, find themselves prey to a relentless alien race in a fight for survival.'
-    },
-    {
-      id: 7,
-      title: 'Train Dreams',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/Train-Dreams.jpg',
-      rating: 8.2,
-      desc: 'Based on Denis Johnson beloved novella, Train Dreams is the moving portrait of Robert Grainier, a logger and railroad worker who leads a life of unexpected depth and beauty in the rapidly-changing America of the early 20th Century.'
-    },
-    {
-      id: 8,
-      title: 'A House of Dynamite',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/A-House-of-Dynamite.jpg',
-      rating: 8.9,
-      desc: 'When a single, unattributed missile is launched at the United States, a race begins to determine who is responsible and how to respond.'
-    },
-    {
-      id: 9,
-      title: 'Welcome to Derry',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/IT-Welcome-to-Derry.jpg',
-      rating: 9.7,
-      desc: 'In 1962, a couple with their son move to Derry, Maine just as a young boy disappears. With their arrival, very bad things begin to happen in the town.'
-    },
-    {
-      id: 10,
-      title: 'The Lost Bus',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/The-Lost-Bus.jpg',
-      rating: 8.8,
-      desc: 'A wayward school bus driver and a dedicated school teacher battle to save 22 children from a terrifying inferno.'
-    },
-    {
-      id: 11,
-      title: 'Samurai Fury',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/Samurai-Fury.jpg',
-      rating: 8.2,
-      desc: 'In 15th century Kyoto before the Onin War, outlaw leader Hyoe uses his deadly swordsmanship to spearhead a rebellion against the corrupt Shogunate and its army, commanded by his former friend and now enemy Doken.'
-    },
-    {
-      id: 12,
-      title: 'Steve',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/Steve.jpg',
-      rating: 9.8,
-      desc: 'Follows headteacher Steve battling for his reform colleges survival while managing his mental health. Concurrently, troubled student Shy navigates his violent tendencies and fragility, torn between his past and future prospects.'
-    },
-    {
-      id: 13,
-      title: 'The Toxic Avenger',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/The-Toxic-Avenger-Movie-Poster-1-610x897.jpg',
-      rating: 7.6,
-      desc: 'A horrible toxic accident transforms downtrodden janitor Winston Gooze into a new evolution of hero: the Toxic Avenger.'
-    },
-    {
-      id: 14,
-      title: 'Eden',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Eden-Movie-Poster-610x904.jpg',
-      rating: 9.1,
-      desc: 'Based on a factual account of a group of outsiders who settle on a remote island only to discover their greatest threat is not the brutal climate or deadly wildlife, but each other.'
-    },
-    {
-      id: 15,
-      title: 'Clash of Empires',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Aztec-Batman-Movie-Poster-1-610x865.jpg',
-      rating: 6.9,
-      desc: 'In an ancient age, the peace-loving Hobbits are enslaved by the Java Men, a race of flesh-eating dragon-riders. The young Hobbit Goben must join forces with their neighbor giants, the humans, to free his people and vanquish their enemies.'
-    },
-    {
-      id: 16,
-      title: 'Americana',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Americana-Movie-Poster-610x763.jpg',
-      rating: 7.9,
-      desc: 'The lives of local outsiders and outcasts violently intertwine when a rare Lakota Ghost shirt falls onto the black market in a small South Dakota town.'
-    },
-    {
-      id: 17,
-      title: 'Honey Dont',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Honey-Dont-Movie-Poster-610x904.jpg',
-      rating: 8.2,
-      desc: 'A small-town private investigator delves into a series of strange deaths tied to a mysterious church.'
-    },
-    {
-      id: 18,
-      title: 'Bloodlines',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/06/Final-Destination-Movie-Poster-610x763.jpg',
-      rating: 8.9,
-      desc: 'A family is forced to face their past secrets and scars when the black sheep returns home.'
-    },
-    {
-      id: 19,
-      title: 'The Final Reckoning',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/08/Mission-Impossible-the-Final-Reckoning-610x904.jpg',
-      rating: 9.7,
-      desc: 'Hunt and the IMF pursue a dangerous AI called the Entity thats infiltrated global intelligence. With governments and a figure from his past in pursuit, Hunt races to stop it from forever changing the world.'
-    },
-    {
-      id: 20,
-      title: 'Star People',
-      img: 'https://www.projectorreviews.com/wp-content/uploads/2025/08/Star-People.jpg',
-      rating: 8.8,
-      desc: 'A photographer receives a tip that could shed light on her childhood UFO sighting, but a heatwave and some unexpected guests threaten to derail her investigation.'
-    },
-    {
-      id: 21,
-      title: 'Black Phone 2',
-      img: 'https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/240922/BP2_Teaser1Sheet6_RGB_1.jpg',
-      rating: 8.2,
-      desc: 'As Finn, now 17, struggles with life after his captivity, his sister begins receiving calls in her dreams from the black phone and seeing disturbing visions of three boys being stalked at a winter camp known as Alpine Lake.'
-    },
-    {
-      id: 22,
-      title: 'The Conjuring',
-      img: 'https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/240617/CJRLR_VERT_MAIN_2764x4096_DOM.jpg',
-      rating: 9.8,
-      desc: 'Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse.'
-    }
+    id: 1,
+    title: 'Use Force',
+    img: 'https://m.media-amazon.com/images/M/MV5BNTgzMjg0NGYtZTI0OC00OWViLWJjOWYtNDVhMjUxMGZhNTgzXkEyXkFqcGc@._V1_.jpg',
+    rating: 8.2,
+    desc: 'Two African American teens become a target of racial profiling by local Law Enforcement.',
+    video: 'https://www.youtube.com/embed/UQI158Mo95I?si=lf5uPOEuhH9GZAdD'
+  },
+  {
+    id: 2,
+    title: 'Tom Hardy',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/04/Havoc.jpg',
+    rating: 9.8,
+    desc: 'With his breakthrough performance as Eames in Christopher Nolan s sci-fi thriller Inception (2010)',
+    video: 'https://www.youtube.com/embed/5zb2DTOSzDM?si=8QFGx--eGluwALy9'
+  },
+  {
+    id: 3,
+    title: 'In Your Dreams',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/In-Your-Dreams-610x903.jpg',
+    rating: 7.6,
+    desc: 'Stevie and her little brother Elliot journey into the wildly absurd landscape...',
+    video: 'https://www.youtube.com/embed/IG1p9fhNIks?si=Q7TvsGsmniioSdy8'
+  },
+  {
+    id: 4,
+    title: 'Monster Island Movie',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/07/Monster-Island-Movie-610x893.jpg',
+    rating: 9.1,
+    desc: 'Set in the Pacific, 1942. A Japanese soldier and a British prisoner of war...',
+    video: 'https://www.youtube.com/embed/Z1V4mciJa8I?si=4xobtw4qok2tm0Hd'
+  },
+  {
+    id: 5,
+    title: 'Frankenstein',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/Frankenstein.jpg',
+    rating: 6.9,
+    desc: 'Dr. Victor Frankenstein, a brilliant but egotistical scientist...',
+    video: 'https://www.youtube.com/embed/8aulMPhE12g?si=YwrLBavVDUuWtrIL'
+  },
+  {
+    id: 6,
+    title: 'Osiris',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/Osiris-610x813.jpg',
+    rating: 7.9,
+    desc: 'Special Forces commandos are abducted mid-operation...',
+    video: 'https://www.youtube.com/embed/IJRqK-y77Jg?si=mz485a2816hRiJRJ'
+  },
+  {
+    id: 7,
+    title: 'Train Dreams',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/11/Train-Dreams.jpg',
+    rating: 8.2,
+    desc: 'Based on Denis Johnson beloved novella, Train Dreams...',
+    video: 'https://www.youtube.com/embed/_Nk8TrBHOrA?si=jc4bcl8x7xcKxVif'
+  },
+  {
+    id: 8,
+    title: 'A House of Dynamite',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/A-House-of-Dynamite.jpg',
+    rating: 8.9,
+    desc: 'When a single, unattributed missile is launched at the United States...',
+    video: 'https://www.youtube.com/embed/_wpw2QHJNco?si=Y39z47KYTY0106Nf'
+  },
+  {
+    id: 9,
+    title: 'Welcome to Derry',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/IT-Welcome-to-Derry.jpg',
+    rating: 9.7,
+    desc: 'In 1962, a couple with their son move to Derry, Maine...',
+    video: 'https://www.youtube.com/embed/oKa6u7LT0qE?si=terUgY1pg_Y7z3m1'
+  },
+  {
+    id: 10,
+    title: 'The Lost Bus',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/The-Lost-Bus.jpg',
+    rating: 8.8,
+    desc: 'A wayward school bus driver and a dedicated school teacher...',
+    video: 'https://www.youtube.com/embed/XSDHjkuwaic?si=a9QaC45lXdx8yYwY'
+  },
+  {
+    id: 11,
+    title: 'Samurai Fury',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/Samurai-Fury.jpg',
+    rating: 8.2,
+    desc: 'In 15th century Kyoto before the Onin War...',
+    video: 'https://www.youtube.com/embed/2jZHPo_zOgI?si=zeOw7Zx_wHVAHMMu'
+  },
+  {
+    id: 12,
+    title: 'Steve',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/10/Steve.jpg',
+    rating: 9.8,
+    desc: 'Follows headteacher Steve battling for his reform college...',
+    video: 'https://www.youtube.com/embed/5L8intrDcM0?si=6AacbFPWCHX4UwdN'
+  },
+  {
+    id: 13,
+    title: 'The Toxic Avenger',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/The-Toxic-Avenger-Movie-Poster-1-610x897.jpg',
+    rating: 7.6,
+    desc: 'A horrible toxic accident transforms downtrodden janitor Winston Gooze...',
+    video: 'https://www.youtube.com/embed/Wki1V-Gxz6M?si=WnjBh-7M847ZDIXI'
+  },
+  {
+    id: 14,
+    title: 'Eden',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Eden-Movie-Poster-610x904.jpg',
+    rating: 9.1,
+    desc: 'Based on a factual account of a group of outsiders who settle on a remote island...',
+    video: 'https://www.youtube.com/embed/w860o1CceTk?si=ySQSFRDTrabILgoZ'
+  },
+  {
+    id: 15,
+    title: 'Clash of Empires',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Aztec-Batman-Movie-Poster-1-610x865.jpg',
+    rating: 6.9,
+    desc: 'In an ancient age, the peace-loving Hobbits are enslaved...',
+    video: 'https://www.youtube.com/embed/-bd_AUSkrRA?si=4WCNqBmM7Y_hEqV8'
+  },
+  {
+    id: 16,
+    title: 'Americana',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Americana-Movie-Poster-610x763.jpg',
+    rating: 7.9,
+    desc: 'The lives of local outsiders and outcasts violently intertwine...',
+    video: 'https://www.youtube.com/embed/ulZoLec80fs?si=uptUtSSArmVDbPnf'
+  },
+  {
+    id: 17,
+    title: 'Honey Dont',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/09/Honey-Dont-Movie-Poster-610x904.jpg',
+    rating: 8.2,
+    desc: 'A small-town private investigator delves into a series of strange deaths...',
+    video: 'https://www.youtube.com/embed/Jzr6pHIZAI0?si=m_QuOs6DCmGOgKvE'
+  },
+  {
+    id: 18,
+    title: 'Bloodlines',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/06/Final-Destination-Movie-Poster-610x763.jpg',
+    rating: 8.9,
+    desc: 'A family is forced to face their past secrets and scars...',
+    video: 'https://www.youtube.com/embed/UWMzKXsY9A4?si=FgPvtotxeY_CfJpw'
+  },
+  {
+    id: 19,
+    title: 'The Final Reckoning',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/08/Mission-Impossible-the-Final-Reckoning-610x904.jpg',
+    rating: 9.7,
+    desc: 'Hunt and the IMF pursue a dangerous AI called the Entity...',
+    video: 'https://www.youtube.com/embed/fsQgc9pCyDU?si=ymdRvUHtXakyWdIF'
+  },
+  {
+    id: 20,
+    title: 'Star People',
+    img: 'https://www.projectorreviews.com/wp-content/uploads/2025/08/Star-People.jpg',
+    rating: 8.8,
+    desc: 'A photographer receives a tip that could shed light...',
+    video: 'https://www.youtube.com/embed/5HeCSenKVmE?si=xAT1OG-pAEHvzDju'
+  },
+  {
+    id: 21,
+    title: 'Black Phone 2',
+    img: 'https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/240922/BP2_Teaser1Sheet6_RGB_1.jpg',
+    rating: 8.2,
+    desc: 'As Finn, now 17, struggles with life after his captivity...',
+    video: 'https://www.youtube.com/embed/DdR-gzFZoDk?si=S3oker203gGWOc98'
+  },
+  {
+    id: 22,
+    title: 'The Conjuring',
+    img: 'https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/240617/CJRLR_VERT_MAIN_2764x4096_DOM.jpg',
+    rating: 9.8,
+    desc: 'Paranormal investigators Ed and Lorraine Warren...',
+    video: 'https://www.youtube.com/embed/bMgfsdYoEEo?si=hcO492u9HL0pNxj2'
+  }
+      
 ];
--- src/router/Dynamic_router.jsx
+++ src/router/Dynamic_router.jsx
@@ -1,40 +1,77 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { item } from '../Data';
+import { Mydata } from '../Data';
 import { Link } from 'react-router-dom';
+
 function Dynamic_router() {
-   
+    const [openTrailer, setOpenTrailer] = useState(false); // <-- ADD THIS
+
     const { id } = useParams();
-    const items = item.find((item) => item.id === parseInt(id));
+    const items = Mydata.find((item) => item.id === parseInt(id));
     if (!items) return <h1>Not found</h1>
+
     return (
         <div className='w-full'>
             <div className='bg-black'>
                 <Link to={`/${items.id}`}>
-                    <section className="relative bg-cover bg-center bg-no-repeat text-white opacity-70 w-full h-[90vh]" style={{ backgroundImage: `url(${items.img})` }}>
-                        <img
-                            src=""
-                            className="w-full h-full object-cover opacity-70"
-                        />
+                    <section
+                        className="relative bg-cover bg-center bg-no-repeat text-white opacity-70 w-full h-[90vh]"
+                        style={{ backgroundImage: `url(${items.img})` }}
+                    >
+                        <img src="" className="w-full h-full object-cover opacity-70" />
 
                         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent flex flex-col justify-center px-10">
                             <h1 className="text-2xl md:text-5xl font-bold max-w-xl leading-tight">
                                 {items.title}
                             </h1>
+
                             <p className="mt-4 text-lg max-w-md opacity-80">
                                 {items.desc}
                             </p>
-                            <button className="mt-6 bg-red-600 cursor-pointer md:w-[200px] px-4 py-2 w-[140px] rounded-lg text-lg font-semibold hover:bg-red-700 transition">
-                                PLay Now
+
+                            <button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setOpenTrailer(true);
+                                }}
+                                className="mt-6 bg-red-600 cursor-pointer md:w-[200px] px-4 py-2 w-[140px] rounded-lg text-lg font-semibold hover:bg-red-700 transition"
+                            >
+                                Play Now
                             </button>
                         </div>
                     </section>
-                </Link>
 
+
+                    {/* ---------------- Trailer Modal ---------------- */}
+                    {openTrailer && (
+                        <div>
+
+                            <div className="fixed inset-0 bg-black/70 bg-opacity-80 flex justify-center items-center z-50">
+                                <div className="bg-black p-4 rounded-lg w-[90%] md:w-[60%]">
+
+                                    <iframe
+                                        className="w-full h-[300px] md:h-[400px]"
+                                        src={items.video}
+                                        title="Trailer"
+                                        allow="autoplay; encrypted-media"
+                                        allowFullScreen
+                                    ></iframe>
+
+                                    <button
+                                        onClick={() => setOpenTrailer(false)}
+                                        className="mt-4 bg-red-600 w-full py-2 rounded-lg text-lg font-semibold hover:bg-red-700"
+                                    >
+                                        Close Trailer
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    )}
+                </Link>
             </div>
-           
         </div>
     )
 }
 
-export default Dynamic_router
+export default Dynamic_router;
